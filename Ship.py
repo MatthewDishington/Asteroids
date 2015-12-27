16d4a9a9d90ea0c7a7973884b1d8865a1ca1737c
MatthewDishington <miffy_d@yahoo.co.uk>
2015-12-27 22:49:12 +0000

    add Asteroids

--- Ship.py
+++ Ship.py
@@ -1,7 +1,8 @@
-__author__ = 'neil'
+__author__ = 'Matthew'
 
 
 from Tkinter import *
+import random
 import math
 
 CANVAS_WIDTH = 400
@@ -63,7 +64,6 @@ class Ship:
         self._update_position()
         self._set_points()
 
-        canvas.delete('all')
         canvas.create_polygon(self.points, fill='', outline='White')
 
     def accelerate(self, event):
@@ -100,6 +100,81 @@ class Ship:
         elif self.y > CANVAS_HEIGHT:
             self.y -= CANVAS_HEIGHT
 
+class Asteroid:
+    def __init__(self):
+        self.minRadius=30
+        self.maxRadius=50
+        self.granularity=20
+        self.minVary=25
+        self.maxVary=75
+
+        self.shape=[]
+        angle=0
+        while angle < 2 * math.pi:
+            angleVaryPc=random.randint(self.minVary, self.maxVary)
+            angleVaryRadians= (2 * math.pi / self.granularity) * angleVaryPc / 100
+            angleFinal = angle + angleVaryRadians - (math.pi / self.granularity)
+            radius= random.randint(self.minRadius, self.maxRadius)
+            point=[0,0]
+            point[0]= math.sin(angleFinal) * radius
+            point[1]= math.cos(angleFinal) * radius
+            self.shape.append(point)
+            angle += 2 * math.pi / self.granularity
+
+
+
+        self.x = random.randint(0,CANVAS_WIDTH)
+        self.y = random.randint(0,CANVAS_HEIGHT)
+        self.angle= random.randint(0,360)
+        self.rotation_factor= random.randint(0,100) / 100.0 - 0.5
+        self.velocity_x = random.randint(0,100) / 100.0 - 0.5
+        self.velocity_y = random.randint(0,100) / 100.0 - 0.5
+
+
+
+    def draw(self, canvas):
+        self._update_position()
+        self._set_points()
+        canvas.create_polygon(self.points, fill='', outline='White')
+
+
+    def _update_position(self):
+        self.points = list()
+
+        # update position based on movement speed (velocity speed)
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+
+        #Keep Asteroid in game world
+        if self.x < 0:
+            self.x += CANVAS_WIDTH
+        elif self.x > CANVAS_WIDTH:
+            self.x -= CANVAS_WIDTH
+
+        if self.y < 0:
+            self.y += CANVAS_HEIGHT
+        elif self.y > CANVAS_HEIGHT:
+            self.y -= CANVAS_HEIGHT
+
+        #Update rotation
+        self.angle += self.rotation_factor
+
+
+    def _set_points(self):
+        radians = self.angle * math.pi /180.0
+        sine = math.sin(radians)
+        cosine = math.cos(radians)
+
+        # calculate point position
+        self.points = list()
+        for p in self.shape:
+            new_x =  p[0] * cosine - p[1] * sine
+            new_y = p[0] * sine + p[1] * cosine
+            self.points.append((self.x + new_x , self.y + new_y))
+
+
+    def remove(self):
+        pass
 
 class Application(Frame):
 
@@ -113,6 +188,9 @@ class Application(Frame):
         self.canvas.pack()
         ship = Ship()
         self.addShip(ship)
+        self.asteroids = list()
+        for i in range(0,3):
+            self.asteroids.append(Asteroid())
 
         parent.bind('<Left>',ship.rotate_left)
         parent.bind('<Right>',ship.rotate_right)
@@ -124,7 +202,10 @@ class Application(Frame):
         self.ship = ship
 
     def draw(self):
+        self.canvas.delete('all')
         self.ship.draw(self.canvas)
+        for asteroid in self.asteroids:
+            asteroid.draw(self.canvas)
         self.parent.after(self.draw_interval, self.draw) # set timer to refresh screen
 
 root = Tk()
